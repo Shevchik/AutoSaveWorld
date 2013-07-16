1382472459efce4b7fcde76a25e4a947adbf4f71
Shevchik <Shev4ik.den@gmail.com>
2013-07-17 00:04:34 +0400

    Finished Factions copy, maybe this will work.
    Currently i can't download factions, so i can't test this.

--- src/autosaveworld/threads/worldregen/FactionsCopy.java
+++ src/autosaveworld/threads/worldregen/FactionsCopy.java
@@ -17,16 +17,26 @@
 
 package autosaveworld.threads.worldregen;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.World;
 
 import com.massivecraft.factions.entity.BoardColls;
 import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.FactionColls;
 import com.massivecraft.mcore.ps.PS;
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.schematic.SchematicFormat;
+import com.sk89q.worldguard.bukkit.BukkitUtil;
 
 import autosaveworld.core.AutoSaveWorld;
 
@@ -41,26 +51,80 @@ public class FactionsCopy {
 		this.wtoregen = Bukkit.getWorld(worldtoregen);
 	}
 	
+	private int taskid;
 	
 	public void copyAllToSchematics()
 	{
 		plugin.debug("Saving factions homes to schematics");
-		//get factions
-		for (Faction f : FactionColls.get().getForWorld(wtoregen.getName()).getAll())
+	    final SchematicFormat format = SchematicFormat.getFormats().iterator().next();
+		final String schemfolder = "plugins/AutoSaveWorld/WorldRegenTemp/Factions/";
+		new File(schemfolder).mkdirs();
+		//save all factions buildings
+		for (final Faction f : FactionColls.get().getForWorld(wtoregen.getName()).getAll())
 		{
 		  	Set<PS> chunks = BoardColls.get().getChunks(f);
 		  	//check if faction has claimed land
 		   	if (chunks.size() != 0)
 		   	{
 		   		//now we will have to iterate over all chunks and find put the bounds
-		    	Chunk cfirst = chunks.iterator().next().asBukkitChunk();
-		    	int xmin = cfirst.getX();int zmin = cfirst.getZ();int xmax=cfirst.getX()+15; int zmax = cfirst.getZ()+15;
+		    	List<Integer> xcoords = new ArrayList<Integer>(); List<Integer> zcoords = new ArrayList<Integer>();
 		    	for (PS ps : chunks)
 		    	{
+		    		xcoords.add(ps.asBukkitChunk().getX());
+		    		zcoords.add(ps.asBukkitChunk().getZ());
 		    		System.out.println(ps.asBukkitChunk().getX());
 		    		System.out.println(ps.asBukkitChunk().getZ());
-		    		ps.asBukkitChunk();
 		    	}
+		    	Collections.sort(xcoords);
+		    	Collections.sort(zcoords);
+		    	//minimal position
+		    	final Vector bvmin = BukkitUtil.toVector(
+		    			new Location(
+		    					wtoregen,
+		    					xcoords.get(0)*16,
+		    					0,
+		    					zcoords.get(0)*16
+		    			)
+		    	);
+		    	final Vector bvmax = BukkitUtil.toVector(
+		    			new Location(
+		    					wtoregen,
+		    					xcoords.get(xcoords.size()-1)*16+15,
+		    					wtoregen.getMaxHeight(),
+		    					zcoords.get(zcoords.size()-1)*16+15
+		    			)
+		    	);
+		    	Runnable copypaste = new Runnable() {
+					public void run(){
+						try {
+						plugin.debug("Saving Faction land "+f.getName()+" to schematic");
+						//copy to clipboard
+						EditSession es = new EditSession(new BukkitWorld(wtoregen),Integer.MAX_VALUE);
+						Vector pos = bvmax;
+						CuboidClipboard clipboard = new CuboidClipboard(
+								bvmax.subtract(bvmin).add(new Vector(1, 1, 1)),
+								bvmin, bvmin.subtract(pos)
+						);
+						clipboard.copy(es);
+						//save to schematic
+				        File schematic = new File(schemfolder + f.getName());
+				        format.save(clipboard, schematic);
+				        plugin.debug("Faction Land "+f.getName()+" saved");
+						} catch (Exception e)
+						{
+							e.printStackTrace();
+						}
+					}
+				};
+				taskid = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, copypaste);
+				while (Bukkit.getScheduler().isCurrentlyRunning(taskid) || Bukkit.getScheduler().isQueued(taskid))
+				{
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 		    }
 		}
 	}
