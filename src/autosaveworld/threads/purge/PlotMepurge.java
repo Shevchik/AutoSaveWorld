b74e18cdee25fd84022a576e79d4345bf661b329
Shevchik <Shev4ik.den@gmail.com>
2013-09-13 15:18:08 +0400

    Rewrite complete
    But this needs testing

--- src/autosaveworld/threads/purge/PlotMepurge.java
+++ src/autosaveworld/threads/purge/PlotMepurge.java
@@ -17,7 +17,6 @@
 
 package autosaveworld.threads.purge;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 import org.bukkit.Bukkit;
@@ -49,52 +48,45 @@ public class PlotMepurge {
 			{
 				plugin.debug("Checking plots in world "+w.getName().toLowerCase());
 				
-				HashSet<Plot> plotstodel = new HashSet<Plot>();
-				
-				HashMap<String, Plot> plotsinfo = PlotManager.getPlots(w);
-				for (final Plot p : plotsinfo.values())
+				//search plot for inactive owners
+				HashSet<Plot> plots = new HashSet<Plot>(PlotManager.getPlots(w).values());
+				for (final Plot p : plots)
 				{
 					plugin.debug("Checking plot " + p.id);
 					
-					
 					if (!pacheck.isActiveCS(p.getOwner()))
 					{
-						plugin.debug("Plot owner is inactive. Adding plot to removal list");
-						plotstodel.add(p);
-					}
-				}
-				
-				for (final Plot p : plotstodel) 
-				{
-					plugin.debug("Purging plot "+p.id);
-					Runnable delPlot = new Runnable()
-					{
-						World thisWorld = w;
-						Plot PlotId = p;
-						public void run()
-						{
-							if (regenplot)
-							{
-								plugin.debug("Regenerating plot "+PlotId.id);
-								PlotManager.clear(thisWorld, PlotId);
-							}
-							plugin.debug("Deleting plot "+PlotId.id);
-							PlotManager.getPlots(thisWorld).remove(PlotId.id);
-							
-							PlotManager.removeOwnerSign(thisWorld, PlotId.id);
-							PlotManager.removeSellSign(thisWorld, PlotId.id);
-												
-							SqlManager.deletePlot(PlotManager.getIdX(PlotId.id), PlotManager.getIdZ(PlotId.id), thisWorld.getName().toLowerCase());
-						}
-					};
-					int taskid = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, delPlot);
+						plugin.debug("Plot owner is inactive. Purging plot "+p.id);
 						
-					//Wait until previous plot regeneration is finished to avoid full main thread freezing
-					while (Bukkit.getScheduler().isCurrentlyRunning(taskid) || Bukkit.getScheduler().isQueued(taskid))
-					{
-						try {Thread.sleep(100);} catch (InterruptedException e) {}
+						Runnable delPlot = new Runnable()
+						{
+							World thisWorld = w;
+							Plot PlotId = p;
+							public void run()
+							{
+								if (regenplot)
+								{
+									plugin.debug("Regenerating plot "+PlotId.id);
+									PlotManager.clear(thisWorld, PlotId);
+								}
+								plugin.debug("Deleting plot "+PlotId.id);
+								PlotManager.getPlots(thisWorld).remove(PlotId.id);
+								
+								PlotManager.removeOwnerSign(thisWorld, PlotId.id);
+								PlotManager.removeSellSign(thisWorld, PlotId.id);
+													
+								SqlManager.deletePlot(PlotManager.getIdX(PlotId.id), PlotManager.getIdZ(PlotId.id), thisWorld.getName().toLowerCase());
+							}
+						};
+						int taskid = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, delPlot);
+							
+						//Wait until previous plot regeneration is finished to avoid full main thread freezing
+						while (Bukkit.getScheduler().isCurrentlyRunning(taskid) || Bukkit.getScheduler().isQueued(taskid))
+						{
+							try {Thread.sleep(100);} catch (InterruptedException e) {}
+						}
+						delplots +=1;
 					}
-					delplots +=1;
 				}
 			}
 		}
