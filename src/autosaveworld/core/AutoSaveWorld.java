a738948e604bf771c005217322c803faaf0c10fa
Shevchik <Shev4ik.den@gmail.com>
2013-12-07 00:56:44 +0400

    This is pointless

--- src/autosaveworld/core/AutoSaveWorld.java
+++ src/autosaveworld/core/AutoSaveWorld.java
@@ -22,7 +22,6 @@ import java.util.logging.Logger;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import autosaveworld.commands.CommandsHandler;
@@ -160,8 +159,6 @@ public class AutoSaveWorld extends JavaPlugin {
 		ch = null;
 		formattingCodesParser = null;
 		constants = null;
-		//unregister handlers
-		HandlerList.unregisterAll(this);
 	}	
 	
 	
