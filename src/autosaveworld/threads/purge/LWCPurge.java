d66ac9b378d8b6884d5152444be1998ad92fbe0c
Shevchik <Shev4ik.den@gmail.com>
2014-02-17 12:18:07 +0400

    Batch lwc protections remove

--- src/autosaveworld/threads/purge/LWCPurge.java
+++ src/autosaveworld/threads/purge/LWCPurge.java
@@ -17,6 +17,8 @@
 
 package autosaveworld.threads.purge;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -38,7 +40,9 @@ public class LWCPurge {
 		this.plugin = plugin;
 	}
 
-	public void doLWCPurgeTask(ActivePlayersList pacheck, boolean delblocks) {
+	public void doLWCPurgeTask(ActivePlayersList pacheck, boolean delblocks) 
+	{
+
 		LWCPlugin lwc = (LWCPlugin) Bukkit.getPluginManager().getPlugin("LWC");
 
 		plugin.debug("LWC purge started");
@@ -50,43 +54,65 @@ public class LWCPurge {
 		{
 			if (!pacheck.isActiveCS(pr.getOwner()))
 			{
-				//delete block
-				if (delblocks)
+				//add protected to delete batch
+				prtodel.add(pr);
+				//delete protections if maximum batch size reached
+				if (prtodel.size() == 80)
 				{
-					Runnable remchest = new Runnable()
-					{
-						Block block = pr.getBlock();
-						@Override
-						public void run()
-						{
-							BlockState bs = block.getState();
-							if (bs instanceof Chest)
-							{
-								((Chest) bs).getBlockInventory().clear();
-							} else
-							if (bs instanceof DoubleChest)
-							{
-								((DoubleChest) bs).getInventory().clear();
-							}
-							block.setType(Material.AIR);
-						}
-					};
-					int taskid = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, remchest);
-
-					while (Bukkit.getScheduler().isCurrentlyRunning(taskid) || Bukkit.getScheduler().isQueued(taskid))
-					{
-						try {Thread.sleep(50);} catch (InterruptedException e) {}
-					}
+					flushBatch(lwc, delblocks);
 				}
-				plugin.debug("Removing protection for inactive player "+pr.getOwner());
-				//delete protections
-				lwc.getLWC().getPhysicalDatabase().removeProtection(pr.getId());
 				//count deleted protections
 				deleted += 1;
 			}
 		}
+		//flush the rest of the batch;
+		flushBatch(lwc, delblocks);
 
 		plugin.debug("LWC purge finished, deleted "+ deleted+" inactive protections");
+	}
+
+	private ArrayList<Protection> prtodel = new ArrayList<Protection>(100);
+	public void flushBatch(LWCPlugin lwc, boolean delblocks)
+	{
+		//delete block
+		if (delblocks)
+		{
+			Runnable remchests = new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					for (Protection pr : prtodel)
+					{
+						plugin.debug("Removing protected block for inactive player "+pr.getOwner());
+						Block block = pr.getBlock();
+						BlockState bs = block.getState();
+						if (bs instanceof Chest)
+						{
+							((Chest) bs).getBlockInventory().clear();
+						} else
+						if (bs instanceof DoubleChest)
+						{
+							((DoubleChest) bs).getInventory().clear();
+						}
+						block.setType(Material.AIR);
+					}
+				}
+			};
+			int taskid = Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, remchests);
+
+			while (Bukkit.getScheduler().isCurrentlyRunning(taskid) || Bukkit.getScheduler().isQueued(taskid))
+			{
+				try {Thread.sleep(50);} catch (InterruptedException e) {}
+			}
+		}
+		for (Protection pr : prtodel)
+		{
+			plugin.debug("Removing protection for inactive player "+pr.getOwner());
+			//delete protections
+			lwc.getLWC().getPhysicalDatabase().removeProtection(pr.getId());
+		}
+		prtodel.clear();
 	}
 
 }
