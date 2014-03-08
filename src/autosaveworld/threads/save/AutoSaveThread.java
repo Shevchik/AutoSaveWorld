ce2a5aa4667adffa686c0c89515cf970f1c06c7d
Shevchik <Shev4ik.den@gmail.com>
2014-03-08 15:46:18 +0400

    Do not save if save is already running

--- src/autosaveworld/threads/save/AutoSaveThread.java
+++ src/autosaveworld/threads/save/AutoSaveThread.java
@@ -89,7 +89,7 @@ public class AutoSaveThread extends Thread {
 	private boolean canSave() {
 		if (plugin.saveInProgress) {
 			plugin.warn("Multiple concurrent saves attempted! Save interval is likely too short!");
-			return true;
+			return false;
 		}
 		if (plugin.backupInProgress) {
 			plugin.warn("AutoBackup is in process. AutoSave cancelled");
