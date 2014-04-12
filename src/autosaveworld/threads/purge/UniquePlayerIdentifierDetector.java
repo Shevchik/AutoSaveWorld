f2047166369c8bcb5db80c3cb1f8957cced8a711
Shevchik <Shev4ik.den@gmail.com>
2014-04-12 15:02:12 +0400

    Actually detect if we are using uuid purge

--- src/autosaveworld/threads/purge/UniquePlayerIdentifierDetector.java
+++ src/autosaveworld/threads/purge/UniquePlayerIdentifierDetector.java
@@ -1,8 +1,21 @@
 package autosaveworld.threads.purge;
 
+import net.minecraft.util.com.mojang.authlib.GameProfile;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+
 public class UniquePlayerIdentifierDetector {
 	
 	public static UniquePlayerIdentifierType getUniquePlayerIdentifierType() {
+		try {
+			Server server = Bukkit.getServer();
+			@SuppressWarnings("deprecation")
+			Class<?> craftofflineplayer = Bukkit.getOfflinePlayer("fakeautopurgeplayer").getClass();
+			craftofflineplayer.getConstructor(server.getClass(), GameProfile.class);
+			return UniquePlayerIdentifierType.UUID;
+		} catch (Exception e) {
+		}
 		return UniquePlayerIdentifierType.NAME;
 	}
 
