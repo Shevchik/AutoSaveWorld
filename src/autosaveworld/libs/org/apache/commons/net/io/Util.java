242d98076c68972d304df050082f0bf7b7135546
Shevchik <Shev4ik.den@gmail.com>
2014-06-13 22:30:09 +0400

    Fix invalid write length

--- src/autosaveworld/libs/org/apache/commons/net/io/Util.java
+++ src/autosaveworld/libs/org/apache/commons/net/io/Util.java
@@ -31,8 +31,9 @@ public final class Util {
 	public static final void copyStream(InputStream source, OutputStream dest) throws IOException {
 		byte[] buf = new byte[4096];
 		try {
-			while (source.read(buf) != -1) {
-				dest.write(buf);
+			int read;
+			while ((read = source.read(buf)) != -1) {
+				dest.write(buf, 0, read);
 			}
 			dest.flush();
 		} catch (IOException e) {
