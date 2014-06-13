47fb362a42bb11353b766541b417c74626db1c4a
Shevchik <Shev4ik.den@gmail.com>
2014-06-13 15:51:32 +0400

    Also delete player stats file

--- src/autosaveworld/threads/purge/byuuids/plugins/DatfilePurge.java
+++ src/autosaveworld/threads/purge/byuuids/plugins/DatfilePurge.java
@@ -16,12 +16,14 @@ public class DatfilePurge {
 		int deleted = 0;
 		String worldfoldername = Bukkit.getWorlds().get(0).getWorldFolder().getAbsolutePath();
 		File playersdatfolder = new File(worldfoldername + File.separator + "playerdata"+ File.separator);
+		File playersstatsfolder = new File(worldfoldername + File.separator + "stats"+ File.separator);
 		for (File playerfile : playersdatfolder.listFiles()) {
 			if (playerfile.getName().endsWith(".dat")) {
 				String playeruuid = playerfile.getName().substring(0, playerfile.getName().length() - 4);
 				if (!pacheck.isActive(playeruuid)) {
 					MessageLogger.debug(playeruuid + " is inactive. Removing dat file");
 					playerfile.delete();
+					new File(playersstatsfolder, playerfile.getName()).delete();
 					deleted += 1;
 				}
 			}
