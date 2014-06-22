7370f0fbd7e96221082f1e59f713253a07f229be
Shevchik <Shev4ik.den@gmail.com>
2014-06-22 17:51:33 +0400

    Add full sftp backup implementation. Still need to test this

--- src/autosaveworld/threads/backup/sftp/SFTPBackupOperations.java
+++ src/autosaveworld/threads/backup/sftp/SFTPBackupOperations.java
@@ -1,27 +1,69 @@
 package autosaveworld.threads.backup.sftp;
 
+import java.io.File;
 import java.util.List;
 
 import org.bukkit.World;
 
+import autosaveworld.core.GlobalConstants;
+import autosaveworld.core.logging.MessageLogger;
 import autosaveworld.zlibs.com.jcraft.jsch.ChannelSftp;
+import autosaveworld.zlibs.com.jcraft.jsch.SftpException;
 
 public class SFTPBackupOperations {
 
+	private final boolean zip;
+	private final List<String> excludefolders;
+	private ChannelSftp sftp;
 	public SFTPBackupOperations(ChannelSftp sftp, boolean zip, List<String> excludeFolders) {
-		
+		this.sftp = sftp;
+		this.zip = zip;
+		this.excludefolders = excludeFolders;
 	}
 
-	public void backupWorld(World w) {
-		
+	public void backupWorld(World world) {
+		MessageLogger.debug("Backuping world "+world.getWorldFolder().getName());
+		boolean savestaus = world.isAutoSave();
+		world.setAutoSave(false);
+		try {
+			File worldfolder = world.getWorldFolder().getAbsoluteFile();
+			backupFolder(worldfolder);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			world.setAutoSave(savestaus);
+		}
+		MessageLogger.debug("Backuped world "+world.getWorldFolder().getName());
 	}
 
 	public void backupPlugins() {
-		
+		try {
+			File plfolder = new File(GlobalConstants.getPluginsFolder()).getAbsoluteFile();
+			backupFolder(plfolder);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void backupOtherFolders(List<String> folders) {
-		
+		for (String folder : folders) {
+			MessageLogger.debug("Backuping folder "+ folder);
+			try {
+				File fld = new File(folder).getAbsoluteFile();
+				backupFolder(fld);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			MessageLogger.debug("Backuped folder "+ folder);
+		}
+	}
+
+	private void backupFolder(File folder) throws SftpException {
+		if (!zip) {
+			SFTPUtils.uploadDirectory(sftp, folder, excludefolders);
+		} else  {
+			SFTPUtils.zipAndUploadDirectory(sftp, folder, excludefolders);
+		}
 	}
 
 }
--- src/autosaveworld/threads/backup/sftp/SFTPUtils.java
+++ src/autosaveworld/threads/backup/sftp/SFTPUtils.java
@@ -1,10 +1,60 @@
 package autosaveworld.threads.backup.sftp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Vector;
+
+import autosaveworld.threads.backup.ExcludeManager;
+import autosaveworld.threads.backup.utils.MemoryZip;
 import autosaveworld.zlibs.com.jcraft.jsch.ChannelSftp;
+import autosaveworld.zlibs.com.jcraft.jsch.ChannelSftp.LsEntry;
+import autosaveworld.zlibs.com.jcraft.jsch.SftpException;
 
 public class SFTPUtils {
 
-	public static void deleteDirectory(ChannelSftp sftp, String oldestBackup) {
+	public static void uploadDirectory(ChannelSftp sftp, File src, List<String> excludefolders) throws SftpException {
+		if (src.isDirectory()) {
+			sftp.mkdir(src.getName());
+			sftp.cd(src.getName());
+			for (File file : src.listFiles()) {
+				if (!ExcludeManager.isFolderExcluded(excludefolders, file.getPath())) {
+					uploadDirectory(sftp, file, excludefolders);
+				}
+			}
+			sftp.cd("..");
+		} else {
+			if (!src.getName().endsWith(".lck")) {
+				try {
+					InputStream is = new FileInputStream(src);
+					sftp.put(is, src.getName());
+					is.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				Thread.yield();
+			}
+		}
+	}
+
+	public static void zipAndUploadDirectory(ChannelSftp sftp, File src, List<String> excludefolders) throws SftpException {
+		InputStream is = MemoryZip.startZIP(src, excludefolders);
+		sftp.put(is, src.getName()+".zip");
+	}
+
+	public static void deleteDirectory(ChannelSftp sftp, String oldestBackup) throws SftpException {
+		Vector<LsEntry> entries = sftp.ls(oldestBackup);
+		for (LsEntry entry : entries) {
+			if (entry.getAttrs().isDir()) {
+				sftp.cd(entry.getFilename());
+				deleteDirectory(sftp, entry.getFilename());
+				sftp.cd("..");
+				sftp.rmdir(entry.getFilename());
+			} else {
+				sftp.rm(entry.getFilename());
+			}
+		}
 	}
 
 }
