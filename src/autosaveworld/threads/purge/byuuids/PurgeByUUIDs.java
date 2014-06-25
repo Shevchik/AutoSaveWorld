4ee488e004ce5ade93e26367b48d0a1d5b4c1ce9
Shevchik <Shev4ik.den@gmail.com>
2014-06-25 21:56:34 +0400

    Add old api purges to uuid based purge

--- src/autosaveworld/threads/purge/byuuids/PurgeByUUIDs.java
+++ src/autosaveworld/threads/purge/byuuids/PurgeByUUIDs.java
@@ -17,14 +17,22 @@
 
 package autosaveworld.threads.purge.byuuids;
 
+import java.util.HashSet;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
 import autosaveworld.config.AutoSaveWorldConfig;
 import autosaveworld.core.AutoSaveWorld;
 import autosaveworld.core.logging.MessageLogger;
+import autosaveworld.threads.purge.WorldEditRegeneration.RegenOptions;
 import autosaveworld.threads.purge.byuuids.plugins.DatfilePurge;
 import autosaveworld.threads.purge.byuuids.plugins.VaultPurge;
+import autosaveworld.threads.purge.byuuids.plugins.oldapi.LWCPurge;
+import autosaveworld.threads.purge.byuuids.plugins.oldapi.MVInvPurge;
+import autosaveworld.threads.purge.byuuids.plugins.oldapi.MyWarpPurge;
+import autosaveworld.threads.purge.byuuids.plugins.oldapi.ResidencePurge;
+import autosaveworld.threads.purge.byuuids.plugins.oldapi.WGPurge;
 
 public class PurgeByUUIDs {
 
@@ -39,7 +47,63 @@ public class PurgeByUUIDs {
 		aplist.gatherActivePlayersList(config.purgeAwayTime * 1000);
 		MessageLogger.debug("Found "+aplist.getActivePlayersCount()+" active players");
 
+		HashSet<Integer> safeids = new HashSet<Integer>();
+		if (config.purgeWERemoveUnsafe) {
+			safeids.addAll(RegenOptions.parseListToIDs(config.purgeWERemoveUnsafeSafeIDs));
+		}
+
 		PluginManager pm = Bukkit.getPluginManager();
+
+
+		//old api purges
+		if (config.purgeUseOldAPI) {
+			if ((pm.getPlugin("WorldGuard") != null) && config.purgeWG) {
+				MessageLogger.debug("WG found, purging");
+				try {
+					new WGPurge().doWGPurgeTask(aplist, config.purgeWGRegenRg, config.purgeWGNoregenOverlap, safeids);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+
+			if ((pm.getPlugin("LWC") != null) && config.purgeLWC) {
+				MessageLogger.debug("LWC found, purging");
+				try {
+					new LWCPurge().doLWCPurgeTask(aplist, config.purgeLWCDelProtectedBlocks);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+
+			if ((pm.getPlugin("Multiverse-Inventories") !=null) && config.purgeMVInv) {
+				MessageLogger.debug("Multiverse-Inventories found, purging");
+				try {
+					new MVInvPurge().doMVInvPurgeTask(aplist);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+
+			if ((pm.getPlugin("Residence") !=null) && config.purgeResidence) {
+				MessageLogger.debug("Residence found, purging");
+				try {
+					new ResidencePurge().doResidencePurgeTask(aplist, config.purgeResidenceRegenArea, safeids);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (pm.getPlugin("MyWarp") != null && config.purgeMyWarp) {
+				MessageLogger.debug("MyWarp found, purging");
+				try {
+					new MyWarpPurge().doMyWarpPurgeTask(aplist);
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		//uuid purges
 
 		if (pm.getPlugin("Vault") != null && config.purgePerms) {
 			MessageLogger.debug("Vault found, purging permissions");
