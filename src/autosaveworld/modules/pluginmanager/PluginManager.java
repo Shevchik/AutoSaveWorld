8cffffe84e9ec1a53fa113ae2af228e722130f7a
Shevchik <Shev4ik.den@gmail.com>
2015-06-28 19:57:04 +0400

    Fix freload command

--- src/autosaveworld/modules/pluginmanager/PluginManager.java
+++ src/autosaveworld/modules/pluginmanager/PluginManager.java
@@ -66,7 +66,7 @@ public class PluginManager {
 				reloadPlugin(sender, arg, false);
 				break;
 			}
-			case "freload:": {
+			case "freload": {
 				reloadPlugin(sender, arg, true);
 				break;
 			}
