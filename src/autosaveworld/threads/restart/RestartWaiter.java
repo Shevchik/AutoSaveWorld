908e577a0e0fdab639f479fe75450d36f0f4b719
Shevchik <Shev4ik.den@gmail.com>
2016-01-20 19:03:07 +0300

    Just use phaser

--- src/autosaveworld/threads/restart/RestartWaiter.java
+++ src/autosaveworld/threads/restart/RestartWaiter.java
@@ -1,71 +1,25 @@
 package autosaveworld.threads.restart;
 
-import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.Phaser;
 
 public class RestartWaiter {
 
 	public static void init() {
 	}
 
-	private static final CountLatch latch = new CountLatch();
+	private static final Phaser phaser = new Phaser();
 
 	public static void await() {
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-		}
+		phaser.register();
+		phaser.arriveAndAwaitAdvance();
 	}
 
 	public static void incrementWait() {
-		latch.countUp();
+		phaser.register();
 	}
 
 	public static void decrementWait() {
-		latch.countDown();
-	}
-
-	public static class CountLatch {
-
-		private static final class Sync extends AbstractQueuedSynchronizer {
-			private static final long serialVersionUID = 1L;
-
-			private Sync() {
-			}
-
-			@Override
-			protected int tryAcquireShared(int acquires) {
-				return getState() == 0 ? 1 : -1;
-			}
-
-			@Override
-			protected boolean tryReleaseShared(int delta) {
-				for (;;) {
-					final int c = getState();
-					final int nextc = c + delta;
-					if (nextc < 0) {
-						return false;
-					}
-					if (compareAndSetState(c, nextc)) {
-						return nextc == 0;
-					}
-				}
-			}
-		}
-
-		private final Sync sync = new Sync();
-
-		public void countUp() {
-			sync.releaseShared(1);
-		}
-
-		public void countDown() {
-			sync.releaseShared(-1);
-		}
-
-		public void await() throws InterruptedException {
-			sync.acquireSharedInterruptibly(1);
-		}
-
+		phaser.arrive();
 	}
 
 }
